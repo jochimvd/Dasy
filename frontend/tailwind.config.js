401652a15e8fbbc5ccecbb0a7bc50f971edd4e73
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2022-10-22 19:28:31 +0200

    Update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: colors.orange,
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.drag-none': {
+          '-webkit-user-drag': 'none',
+          '-khtml-user-drag': 'none',
+          '-moz-user-drag': 'none',
+          '-o-user-drag': 'none',
+          'user-drag': 'none',
+        },
+      })
+    })
+  ],
 }
