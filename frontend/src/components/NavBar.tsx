5dd3a5e26639e37ba92a48e65fbbc86a708e6e03
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2022-12-08 21:51:22 +0100

    Remove Home and add Reports to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,9 +7,9 @@ import { clickOutside } from "../utils/Directives";
 import { BellOutline, MenuOutline, PlusS, XOutline } from "../utils/Icons";
 
 const navigation = [
-  { name: "Home", href: "/", end: true },
   { name: "Observations", href: "/observations" },
   { name: "Board", href: "/board" },
+  { name: "Reports", href: "/reports" },
   { name: "Configuration", href: "/configuration" },
 ];
 
@@ -99,7 +99,6 @@ const NavBar = () => {
                         class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                         activeClass="border-orange-500 text-gray-900"
                         inactiveClass="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        end={item.end}
                       >
                         {item.name}
                       </A>
@@ -215,7 +214,6 @@ const NavBar = () => {
                       class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
                       activeClass="bg-orange-50 border-orange-500 text-orange-700"
                       inactiveClass="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"
-                      end={item.end}
                       // onClick={() => setMobileNavMenu(false)}
                     >
                       {item.name}
