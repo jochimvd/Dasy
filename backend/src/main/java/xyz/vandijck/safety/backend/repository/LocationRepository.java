9e4b3ccc03e83bc8c7ae9d22ce5af38f850e09c8
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2022-12-20 16:58:27 +0100

    Remove/rename

--- backend/src/main/java/xyz/vandijck/safety/backend/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package xyz.vandijck.safety.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.vandijck.safety.backend.entity.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Long>
-{
-}
--- backend/src/main/java/xyz/vandijck/safety/backend/service/ConsequenceService.java
+++
@@ -1,7 +0,0 @@
-package xyz.vandijck.safety.backend.service;
-
-import xyz.vandijck.safety.backend.entity.Consequence;
-import xyz.vandijck.safety.backend.request.ConsequenceSearchRequest;
-
-public interface ConsequenceService extends EntityService<Consequence, ConsequenceSearchRequest> {
-}
