73c44cce6a5d40b6f0719a71ffcc9d7873f90fa8
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2022-12-20 17:30:00 +0100

    Add dummy notification threshold field

--- frontend/src/pages/configuration/General.tsx
+++ frontend/src/pages/configuration/General.tsx
@@ -7,7 +7,7 @@ const General = () => {
   return (
     <ConfigurationEditLayout
       heading={"General Configuration"}
-      subheading={"Update the servers' general configuration."}
+      subheading={"Update the applications' general configuration."}
       saving={saving}
       onSubmit={() => {}}
     >
@@ -23,6 +23,24 @@ const General = () => {
             id="observation-start"
             type="datetime-local"
             class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+            value={"2022-05-01T00:00:00"}
+          />
+        </div>
+      </div>
+
+      <div class="mt-6 grid grid-cols-4 gap-6">
+        <div class="col-span-4 sm:col-span-2">
+          <label
+            for="notification-threshold"
+            class="block text-sm font-medium text-gray-700"
+          >
+            Notification threshold
+          </label>
+          <input
+            id="notification-threshold"
+            type="number"
+            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+            value={"20"}
           />
         </div>
       </div>
